bd3e74e9a234f639a413b42083a9affe593bacb3
VIPUL SINGH <120162007+VIPULSINGH18@users.noreply.github.com>
2024-06-15 17:32:18 +0530

    Add files via upload

---
+++ Lec7- CipherSchools.cpp
@@ -0,0 +1,31 @@
+// this code is about reversing the input numbers without using array
+
+
+#include<iostream>
+using namespace std;
+int main(){
+	int a,b,c,d,e;
+	cout<<"Enter 5 values:";
+	cin>>a>>b>>c>>d>>e;
+	cout<<"Reverse of input numbers:";
+	cout<<e<<" "<<d<<" " <<c<<" " <<b<<" "<<a ;
+	return 0;
+	
+}
+
+
+
+
+
+#include<iostream>
+using namespace std;
+int main(){
+	int x;  // this is variable x of int data type which can store only single integer value
+	       // arrays can be used to store multiple values
+	int array[5];  // it is an array which can store multiple values 
+				  //  indexing of an array starts from 0
+				  // in above array there are total 6 elements inside it
+				  // First element of an array having index a[0]
+				  // Last element of an array having index a[5]        
+	       
+} 
